af38c4c9a94ade665f2c952a342bf5340b72822f
anusha jakka <anusha_2227@yahoo.co.in>
2017-08-09 14:31:59 +0530

    changes on testconfig

--- src/com/Datalicious/Config/TestConfig.java
+++ src/com/Datalicious/Config/TestConfig.java
@@ -9,9 +9,6 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
-
-import com.gargoylesoftware.htmlunit.WebConsole.Logger;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
