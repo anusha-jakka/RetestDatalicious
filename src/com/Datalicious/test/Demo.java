140cf66e0b10834457f8e03b010dd8637cbbfe72
anusha jakka <anusha_2227@yahoo.co.in>
2017-08-09 15:02:43 +0530

    new and update file

--- src/com/Datalicious/test/Demo.java
+++ src/com/Datalicious/test/Demo.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
-//comment added
+
 public class Demo {
 	
 	WebDriver driver;
---
+++ src/com/Datalicious/test/branchCheck.java
@@ -0,0 +1,9 @@
+package com.Datalicious.test;
+
+class branchCheck
+{
+	public static void main(String args[])
+	{
+		System.out.println("In new Branch");
+	}
+	}
